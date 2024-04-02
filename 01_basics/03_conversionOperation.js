c56a8fff71e926b6d5344261ead578818c6d1fd5
Ayush Gupta <144621375+ayushgupta20@users.noreply.github.com>
2024-04-02 17:03:38 +0000

    datatype conversion and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,6 +12,39 @@ true => 1; false => 0
 "ayush" => true
 */
 
-let isloggedIn =1;
-let booleanIsLoggedIn = Boolean(isloggedIn);
-console.log(booleanIsLoggedIn);
+// let isloggedIn =1;
+// let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(booleanIsLoggedIn);
+// ************************* Operations******************
+// let value = 3 ;
+// let negValue= -value;
+// console.log(negValue);
+
+// let str1 = "ayush";
+// let str2 = " gupta"
+// let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1"+2)
+console.log(1+2+"4")
+// console.log(str3)
+console.log((3+4)*(5% 3))
+
+console.log(true+1)
+console.log(+true);
+let test = +"";    //number
+console.log(typeof test);
+
+let num1, num2, num3
+num1=num2=num3=2+2
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+let str = "1"
+let num =1;
+if(num===str){
+    console.log(true);
+}else{
+    console.log(false);
+}

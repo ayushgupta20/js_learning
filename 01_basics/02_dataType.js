76180fd73945604f3b739a1609e40526dd942371
Ayush Gupta <144621375+ayushgupta20@users.noreply.github.com>
2024-04-02 06:16:42 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,19 @@
+// datatypes
+"use strict";  //treat all code as new version of js
+// alert(3+2) // We are using node js
+
+let name ="ayush";
+let age = 22;
+let isLoggedIn=false;
+
+// number => 2 to power 53
+// bigint (used in trading ,stockmarket, big companies like fb)
+// string=>""
+// boolean => true/false
+// null => standalone value   [IT TYPE IS OBJECT]
+// undefined => value is not defined 
+// symbol => unique
+
+// objects
+console.log(typeof undefined)   //undefined
+console.log(typeof null)        //object

bb5f3c87160b5953432400706c0f504930f09f00
Ayush Gupta <144621375+ayushgupta20@users.noreply.github.com>
2024-04-08 06:36:28 +0000

    for in loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,23 @@
+//for of loop
+
+// const detail = [
+//     {
+//         name:"ayush",
+//         age:33,
+//         gmail:"@.com"
+//     },
+//     {
+//         name:"ayush",
+//         age:33,
+//         gmail:"@.com"
+//     },
+//     {
+//         name:"ayush",
+//         age:33,
+//         gmail:"@.com"
+//     }
+// ]
+
+// for (const obj of detail) {
+//     console.log(obj.age)
+// }
